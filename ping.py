093a2fde3f14eab215418724f84ca9700b1166a7
HitBoyXx23-dev <hitboyxx23@proton.me>
2026-02-03 09:21:53 +0900

    Update ping.py

--- ping.py
+++ ping.py
@@ -13,6 +13,7 @@ URLS_TO_PING = [
     "https://navcloud.onrender.com",
     "https://navine-file-sharer.onrender.com",
     "https://website-pinger-w81z.onrender.com",
+    "https://euphoria-ai-bot.onrender.com",
     "example.com",
     "example.com",
 ]
